cb4c1ac8823d62b779e4b668ee31efc84013d94f
ginger <144500639+ginger-lau@users.noreply.github.com>
2023-11-02 23:18:33 +0000

    Nov 2, 2023, 4:18 PM

---
+++ homeworks/hw7/assets/js/script.js
@@ -0,0 +1,22 @@
+function averageThreeNumbers(a, b, c) {
+    let sum = a+b+c;
+    let avg = sum /3;
+    return avg;
+}
+
+/* OR THIS
+function createSentence(num, noun){
+    let sentence = "On average, a Berkeley student has "+num+" "+noun+"s";
+    return sentence;
+}
+*/
+
+//I dont have JS experience, I was just curious
+function createSentence(num, noun){
+    return `On average, a Berkeley student has ${num} ${noun}s.`;
+}
+
+function getRandomNum(max){
+    randNum = Math.random() * max; // Math.random() gives [1,0] therefore multiply by max to get a max rand value
+    return randNum;
+}
