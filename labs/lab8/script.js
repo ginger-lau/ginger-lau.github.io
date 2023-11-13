6d3295a9630de0e22c6e507c8e61eef22d02b16d
ginger <144500639+ginger-lau@users.noreply.github.com>
2023-11-13 01:47:41 +0000

    Nov 12, 2023, 5:47 PM

--- labs/lab8/script.js
+++ labs/lab8/script.js
@@ -1,7 +1,8 @@
-function toggleTheme(){
- let body = document.body;
- body.classList.toggle(".dark-mode");
+//toggleTheme switches the "dark-mode" of entire body when run
+function toggleTheme(){ 
+ var body = document.body              //selecting body
+ body.classList.toggle("dark-mode")    //assign toggle "dark-mode" to body
 }
 
-let buttonVariable = document.getElementById("toggleButton");
-buttonVariable.onclick = toggleTheme;
+let buttonVariable = document.getElementById("toggleButton");             // set var to selection button with toggleButton ID
+buttonVariable.onclick = toggleTheme;                           // when toggleButton is clicked, run toggleTheme funct
