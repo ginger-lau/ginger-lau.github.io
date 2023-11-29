9af1c830858533672e582361f8a9e31fc63dcd83
ginger <144500639+ginger-lau@users.noreply.github.com>
2023-11-29 16:22:51 +0000

    Nov 29, 2023, 8:22 AM

--- final/assets/js/script.js
+++ final/assets/js/script.js
@@ -1,19 +1,17 @@
 function toggleProjects(){
-    let songsProjectHalf = document.getElementById("cards-second-half");
-    songsProjectHalf.classList.toggle("hidden");
-}
-
-document.getElementById("toggle-button").onclick = toggleProjects;
-
+    let cardsSecondHalf = document.getElementById("cards-second-half");
+    cardsSecondHalf.classList.toggle("hidden"); 
+  }
+  
+  document.getElementById("toggle-button").onclick = toggleProjects;
 // Get projects-text element 
 const projectsText = document.querySelector('.projects-text');
 
 
 projectsText.style.opacity = 0; 
 
-
 window.addEventListener('load', () => {
-  const fadeEffect = setInterval(function () {
+  const fadeEffect = setInterval(function() {
   
     if(projectsText.style.opacity == 1){
       clearInterval(fadeEffect);
