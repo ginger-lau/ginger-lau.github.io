56111a0b60e8c5d8630d8fd01d9ba2a8e4dd2153
ginger <144500639+ginger-lau@users.noreply.github.com>
2023-11-29 16:24:36 +0000

    Nov 29, 2023, 8:24 AM

--- final/assets/js/script.js
+++ final/assets/js/script.js
@@ -4,19 +4,3 @@ function toggleProjects(){
   }
   
   document.getElementById("toggle-button").onclick = toggleProjects;
-// Get projects-text element 
-const projectsText = document.querySelector('.projects-text');
-
-
-projectsText.style.opacity = 0; 
-
-window.addEventListener('load', () => {
-  const fadeEffect = setInterval(function() {
-  
-    if(projectsText.style.opacity == 1){
-      clearInterval(fadeEffect);
-    }
-   
-    projectsText.style.opacity = +projectsText.style.opacity + 0.1; 
-  }, 50);
-})
